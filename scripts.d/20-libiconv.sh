4d74e9f00c2dd14e8b6c95564a16ba0d63e2a1f6
Alexei T. <Lichtenshtein@users.noreply.github.com>
2025-10-18 13:42:49 +0000

    Update 20-libiconv.sh

--- scripts.d/20-libiconv.sh
+++ scripts.d/20-libiconv.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-#SCRIPT_REPO="https://https.git.savannah.gnu.org/git/libiconv.git"
-#SCRIPT_MIRROR="git://git.git.savannah.gnu.org/libiconv.git"
-#SCRIPT_COMMIT="61fbe742a3702e5030f8258ad130a847d6c3e89a"
-
-SCRIPT_REPO="https://git.savannah.gnu.org/git/libiconv.git"
-SCRIPT_COMMIT="918904edaf378253f262ce19da02ec69cec94a4e"
+SCRIPT_REPO="https://https.git.savannah.gnu.org/git/libiconv.git"
+SCRIPT_MIRROR="git://git.git.savannah.gnu.org/libiconv.git"
+SCRIPT_COMMIT="61fbe742a3702e5030f8258ad130a847d6c3e89a"
 
 SCRIPT_REPO2="https://https.git.savannah.gnu.org/git/gnulib.git"
-SCRIPT_COMMIT2="19b6bc71b09d8b1a342a9d529ee6ab117b04dd7a"
-
-SCRIPT_REPO3="https://https.git.savannah.gnu.org/git/gnulib.git"
-SCRIPT_MIRROR3="https://github.com/coreutils/gnulib.git"
-SCRIPT_COMMIT3="ef6d228c7dba8363ce147450c3c2c3ab95642d4c"
+SCRIPT_MIRROR2="https://github.com/coreutils/gnulib.git"
+SCRIPT_COMMIT2="ef6d228c7dba8363ce147450c3c2c3ab95642d4c"
 
 ffbuild_enabled() {
     return 0
