57af25fd0aa223caa255b9e69b3cd2d50d40e527
Alexei T. <Lichtenshtein@users.noreply.github.com>
2025-10-21 00:47:17 +0000

    Update 50-libcaca-test.sh

--- scripts.d/50-libcaca-test.sh
+++ scripts.d/50-libcaca-test.sh
@@ -4,7 +4,7 @@ SCRIPT_REPO="https://github.com/chiefjazzdiewltr/libcaca.git"
 
 ffbuild_enabled() {
     [[ $TARGET == win* ]] || return 1
-    return 0
+    return -1
 }
 
 # i have no idea what i'm doing
