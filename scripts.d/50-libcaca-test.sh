710aa9c1b44723e4b106c39532a0f4805ef3d8be
Alexei T. <Lichtenshtein@users.noreply.github.com>
2025-10-22 08:24:35 +0000

    Update 50-libcaca-test.sh

--- scripts.d/50-libcaca-test.sh
+++ scripts.d/50-libcaca-test.sh
@@ -14,17 +14,39 @@ ffbuild_dockerbuild() {
     ./bootstrap
 
 # apt-get install -y libx11 xorgproto libncurses5-dev
-apt-get install -y libncurses5-dev
+apt-get install -y libncurses5-dev mesa-common-dev libgl-dev libglu-dev freeglut3-dev pkg-config libtool
 # git clone --depth=1 https://github.com/chiefjazzdiewltr/libcaca.git
 
+	git clone https://github.com/freeglut/freeglut.git
+    mkdir build
+    cd build
+    cmake .. -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX"
+    make -j$(nproc)
+    make install DESTDIR="$FFBUILD_DESTDIR"
+
+    git clone https://gitlab.freedesktop.org/xorg/proto/xorgproto.git
+    cd xorgproto
+    ./autogen.sh
+    ./configure --prefix=="$FFBUILD_PREFIX" --host="$FFBUILD_TOOLCHAIN" --disable-shared --enable-static
+	make -j$(nproc)
+    make install DESTDIR="$FFBUILD_DESTDIR"
+
+	git clone https://gitlab.freedesktop.org/xorg/lib/libX11.git
+    cd libX11
+	./autogen.sh
+	./configure --prefix="$FFBUILD_PREFIX" --host="$FFBUILD_TOOLCHAIN" --disable-shared --enable-static
+	make -j$(nproc)
+    make install DESTDIR="$FFBUILD_DESTDIR"
+	
      local myconf=(
         --prefix="$FFBUILD_PREFIX"
         --host="$FFBUILD_TOOLCHAIN"
         --disable-shared
         --enable-static
         --enable-ncurses
-#		--enable-imlib2
+		--enable-imlib2
 		--enable-gl
+		--enable-freeglu
         --disable-extra-programs
         --disable-csharp 
         --disable-java 
