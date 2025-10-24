850463d184411543203d7e3b50c9d3707711dac3
Alexei T. <Lichtenshtein@users.noreply.github.com>
2025-10-24 05:30:53 +0000

    Update 50-vapoursynth-test.sh

--- scripts.d/50-vapoursynth-test.sh
+++ scripts.d/50-vapoursynth-test.sh
@@ -4,23 +4,22 @@ SCRIPT_REPO="https://github.com/vapoursynth/vapoursynth.git"
 
 ffbuild_enabled() {
     [[ $TARGET == win* ]] || return 1
-    return -1
+    return 0
 }
 
 # i have no idea what i'm doing
 
 ffbuild_dockerbuild() {
 
-apt-get install -y libzimg-dev zimg autoconf automake libtool g++ git pkg-config build-essential python3-dev unzip 
+apt-get install -y libzimg-dev zimg autoconf automake libtool g++ pkg-config build-essential python3-dev unzip 
 pip3 install Cython
 
-git clone --depth=1 https://github.com/vapoursynth/vapoursynth.git
+# git clone --depth=1 https://github.com/vapoursynth/vapoursynth.git
 cd vapoursynth
 ./autogen.sh
-./configure
+./configure --prefix=="$FFBUILD_PREFIX" --host="$FFBUILD_TOOLCHAIN" --disable-shared --enable-static
 make -j$(nproc)
 make install DESTDIR="$FFBUILD_DESTDIR"
-cd ..
 
 }
 
