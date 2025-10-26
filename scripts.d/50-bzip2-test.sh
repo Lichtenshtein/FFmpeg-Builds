bfbbe64188df3265a97e592bf2ceadbcc585fd0a
Alexei T. <Lichtenshtein@users.noreply.github.com>
2025-10-26 05:37:34 +0000

    Update 50-bzip2-test.sh

--- scripts.d/50-bzip2-test.sh
+++ scripts.d/50-bzip2-test.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/ImageMagick/bzip2.git"
+SCRIPT_COMMIT="abffe764f875f71d051efb19d4c83139375f82d7"
 
 ffbuild_enabled() {
     [[ $TARGET == win* ]] || return 1
-    return -1
+    return 0
 }
 
 # i have no idea what i'm doing
 
 ffbuild_dockerbuild() {
 
-# apt-get install libbz2-dev
-# git clone --depth=1 https://github.com/ImageMagick/bzip2.git
-# cd bzip2
-./configure --prefix=="$FFBUILD_PREFIX" --host="$FFBUILD_TOOLCHAIN" --disable-shared --enable-static
-make -j$(nproc)
-make install DESTDIR="$FFBUILD_DESTDIR"
-# cd ..
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --disable-shared
+        --enable-static
+    )
+
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    export CPPFLAGS="$CPPFLAGS -I$FFBUILD_PREFIX/include"
+
+    ./configure "${myconf[@]}"
+    make -j$(nproc)
+    make install DESTDIR="$FFBUILD_DESTDIR"
   
 }
 
