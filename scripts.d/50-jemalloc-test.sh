5e9014eb55d2d7d0fc0004339dd5f48822b0bb39
Alexei T. <Lichtenshtein@users.noreply.github.com>
2025-10-27 14:14:02 +0000

    Update 50-jemalloc-test.sh

--- scripts.d/50-jemalloc-test.sh
+++ scripts.d/50-jemalloc-test.sh
@@ -15,6 +15,7 @@ ffbuild_dockerbuild() {
         --prefix="$FFBUILD_PREFIX"
         --disable-shared
         --enable-static
+        --with-jemalloc-prefix=je_
         --disable-initial-exec-tls
         --with-lg-quantum=3
         --enable-autogen
@@ -30,10 +31,29 @@ ffbuild_dockerbuild() {
     fi
 
     export CPPFLAGS="$CPPFLAGS -I$FFBUILD_PREFIX/include"
+    
+    echo "Libs.private: @LIBS@" >> jemalloc.pc.in
+    echo "jemalloc_prefix=@JEMALLOC_PREFIX@" >> jemalloc.pc.in
+    CFLAGS="${CFLAGS/-fPIC/}"
+    CFLAGS="${CFLAGS/-DPIC/}"
+    export CFLAGS="${CFLAGS/-fno-semantic-interposition/} -fPIE"
+    CXXFLAGS="${CXXFLAGS/-fPIC/}"
+    CXXFLAGS="${CXXFLAGS/-DPIC/}"
+    export CXXFLAGS="${CXXFLAGS/-fno-semantic-interposition/} -fPIE"
 
     ./autogen.sh "${myconf[@]}"
-    make -j$(nproc)
-    make install DESTDIR="$FFBUILD_DESTDIR"
+    make -j$(nproc) build_lib_static
+    make DESTDIR="$FFBUILD_DESTDIR" install_include install_lib_static install_lib_pc
+
+#    ./autogen.sh "${myconf[@]}"
+#    make -j$(nproc)
+#    make install DESTDIR="$FFBUILD_DESTDIR"
+
+    if [[ $VARIANT == *shared* ]]; then
+        mv "$FFBUILD_PREFIX"/lib/libjemalloc{_pic,}.a
+    else
+        rm "$FFBUILD_PREFIX"/lib/libjemalloc_pic.a
+    fi
 
 }
 
