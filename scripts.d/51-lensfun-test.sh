0a9101bbbbc80cca8f1c1c9ac78a66bdddd3c34a
Alexei T. <Lichtenshtein@users.noreply.github.com>
2025-10-29 08:45:45 +0000

    Update 51-lensfun-test.sh

--- scripts.d/51-lensfun-test.sh
+++ scripts.d/51-lensfun-test.sh
@@ -6,13 +6,18 @@ SCRIPT_COMMIT="101c745e847a5de4a1e569a94368ce2027198598"
 # SCRIPT_REPO="https://github.com/Lichtenshtein/lensfun.git"
 # SCRIPT_COMMIT="60b39ece3464ba6a730e5e7f5f8221e89c9a72c9"
 
-# #229 0.875 -- Looking for include file endian.h - not found
-# #229 0.882 -- Found PkgConfig: /usr/bin/pkg-config (found version "1.8.1") 
-# #229 0.883 -- Checking for one of the modules 'glib-2.0'
-# #229 0.899 CMake Error at cmake/modules/FindGLIB2.cmake:69 (MESSAGE):
-# #229 0.899   Could not find glib2
-# #229 0.899 Call Stack (most recent call first):
-# #229 0.899   CMakeLists.txt:116 (FIND_PACKAGE)
+#229 4.086 make[2]: *** [libs/regex/CMakeFiles/tre_regex.dir/build.make:107: libs/regex/CMakeFiles/tre_regex.dir/regexec.c.obj] Error 1
+#229 4.090 make[2]: *** [libs/regex/CMakeFiles/tre_regex.dir/build.make:77: libs/regex/CMakeFiles/tre_regex.dir/regcomp.c.obj] Error 1
+#229 4.090 make[1]: *** [CMakeFiles/Makefile2:149: libs/regex/CMakeFiles/tre_regex.dir/all] Error 2
+#229 4.090 make[1]: *** Waiting for unfinished jobs....
+#229 4.167 running build
+#229 4.168 running build_py
+#229 4.170 creating build
+#229 4.170 creating build/lib
+#229 4.171 creating build/lib/lensfun
+#229 4.171 copying /51-lensfun-test/build/apps/lensfun/__init__.py -> build/lib/lensfun
+#229 4.202 [ 13%] Built target python-package
+#229 4.203 make: *** [Makefile:156: all] Error 2
 
 ffbuild_enabled() {
     [[ $TARGET == win* ]] || return 1
@@ -24,9 +29,9 @@ ffbuild_enabled() {
 ffbuild_dockerbuild() {
 
 #    apt-get install -y libgtk-3-dev
-#    apt-get install -y libglib2.0-dev
+#    apt-get install -y libglib2.0-dev libxml2-utils python3-dev
 
-apt-get install -y libpng-dev python3-dev libxml2-utils
+apt-get install -y libpng-dev libsystre 
 
 # stupid blind fuck can't find glib
 # dpkg -L libglib2.0-dev
@@ -42,10 +47,9 @@ apt-get install -y libpng-dev python3-dev libxml2-utils
         return -1
     fi
 
-    export CPPFLAGS="$CPPFLAGS -I$FFBUILD_PREFIX/include"
+#    export CPPFLAGS="$CPPFLAGS -I$FFBUILD_PREFIX/include"
     export PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/lib/x86_64-linux-gnu/glib-2.0:/usr/include/glib-2.0:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu/glib-2.0/include:$PKG_CONFIG_PATH"
     
-
     mkdir build
     cd build
     cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DINSTALL_HELPER_SCRIPTS=off -DBUILD_TESTS=OFF -DBUILD_DOC=OFF -DBUILD_FOR_SSE2=ON -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DBUILD_STATIC=on -DBUILD_SHARED_LIBS=NO ..
