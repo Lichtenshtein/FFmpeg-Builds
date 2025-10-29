22a9efe19390a607a229138b57853785d20dc276
Alexei T. <Lichtenshtein@users.noreply.github.com>
2025-10-29 05:57:39 +0000

    Update 51-lensfun-test.sh

--- scripts.d/51-lensfun-test.sh
+++ scripts.d/51-lensfun-test.sh
@@ -13,7 +13,7 @@ SCRIPT_COMMIT="60b39ece3464ba6a730e5e7f5f8221e89c9a72c9"
 
 ffbuild_enabled() {
     [[ $TARGET == win* ]] || return 1
-    return -1
+    return 0
 }
 
 # i have no idea what i'm doing
@@ -22,6 +22,8 @@ ffbuild_dockerbuild() {
 
 #    apt-get install -y libgtk-3-dev
 #    apt-get install -y libglib2.0-dev
+
+apt-get install -y libpng-dev python3-dev libxml2-utils
 
 # stupid blind fuck can't find glib
 # dpkg -L libglib2.0-dev
@@ -38,15 +40,13 @@ ffbuild_dockerbuild() {
     fi
 
     export CPPFLAGS="$CPPFLAGS -I$FFBUILD_PREFIX/include"
-
-    export PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/lib/x86_64-linux-gnu/glib-2.0:$PKG_CONFIG_PATH"
     
 #    CFLAGS += $(pkg-config glib-2.0 --cflags)
 #    LDLIBS += $(pkg-config glib-2.0 --libs)
     
     mkdir build
     cd build
-    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/glib-2.0 -DCMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu/glib-2.0 -DCMAKE_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu -DCMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu -DCMAKE_BUILD_TYPE=Release -DINSTALL_HELPER_SCRIPTS=off -DBUILD_TESTS=OFF -DBUILD_DOC=OFF -DBUILD_FOR_SSE2=ON -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DBUILD_STATIC=on -DBUILD_SHARED_LIBS=NO ..
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DINSTALL_HELPER_SCRIPTS=off -DBUILD_TESTS=OFF -DBUILD_DOC=OFF -DBUILD_FOR_SSE2=ON -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DBUILD_STATIC=on -DBUILD_SHARED_LIBS=NO ..
     make -j$(nproc)
     make install DESTDIR="$FFBUILD_DESTDIR"
 
