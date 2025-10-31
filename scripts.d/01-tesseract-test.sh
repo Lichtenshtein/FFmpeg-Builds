997e18fd73f8ea85c910a20b84ad7c451403200b
Alexei T. <Lichtenshtein@users.noreply.github.com>
2025-10-31 04:54:51 +0000

    Update 01-tesseract-test.sh

--- scripts.d/01-tesseract-test.sh
+++ scripts.d/01-tesseract-test.sh
@@ -10,6 +10,29 @@ ffbuild_enabled() {
 
 # i have no idea what i'm doing
 
+# Build FFmpeg #115 attempt
+#10 49.52 make[1]: Entering directory '/01-tesseract-test'
+#10 49.55   CXX      src/arch/libtesseract_native_la-dotproduct.lo
+#10 49.55   CXX      src/arch/libtesseract_avx_la-dotproductavx.lo
+#10 49.56   CXX      src/arch/libtesseract_avx2_la-intsimdmatrixavx2.lo
+#10 49.56   CXX      src/arch/libtesseract_avx512_la-dotproductavx512.lo
+#10 49.64 x86_64-w64-mingw32-g++: warning: /usr/include/leptonica:: linker input file unused because linking not done
+#10 49.64 x86_64-w64-mingw32-g++: error: /usr/include/leptonica:: linker input file not found: No such file or directory
+#10 49.64 make[1]: *** [Makefile:6426: src/arch/libtesseract_native_la-dotproduct.lo] Error 1
+#10 49.64 make[1]: *** Waiting for unfinished jobs....
+#10 50.41 x86_64-w64-mingw32-g++: warning: /usr/include/leptonica:: linker input file unused because linking not done
+#10 50.41 x86_64-w64-mingw32-g++: error: /usr/include/leptonica:: linker input file not found: No such file or directory
+#10 50.41 make[1]: *** [Makefile:6195: src/arch/libtesseract_avx512_la-dotproductavx512.lo] Error 1
+#10 50.44 x86_64-w64-mingw32-g++: warning: /usr/include/leptonica:: linker input file unused because linking not done
+#10 50.44 x86_64-w64-mingw32-g++: error: /usr/include/leptonica:: linker input file not found: No such file or directory
+#10 50.45 make[1]: *** [Makefile:6181: src/arch/libtesseract_avx_la-dotproductavx.lo] Error 1
+#10 50.47 x86_64-w64-mingw32-g++: warning: /usr/include/leptonica:: linker input file unused because linking not done
+#10 50.47 x86_64-w64-mingw32-g++: error: /usr/include/leptonica:: linker input file not found: No such file or directory
+#10 50.47 make[1]: *** [Makefile:6188: src/arch/libtesseract_avx2_la-intsimdmatrixavx2.lo] Error 1
+#10 50.47 make[1]: Leaving directory '/01-tesseract-test'
+#10 50.47 make: *** [Makefile:7990: all-recursive] Error 1
+#10 ERROR: process "/bin/sh -c run_stage /stage.sh" did not complete successfully: exit code: 2
+
 ffbuild_dockerbuild() {
 
 # main
@@ -47,7 +70,8 @@ find / -name "archive.h" 2>/dev/null
 
 export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/usr/include/leptonica"
 export C_INCLUDE_PATH="$C_INCLUDE_PATH:/usr/include/leptonica"
-export CPPFLAGS="$CPPFLAGS -I$FFBUILD_PREFIX/include -I$FFBUILD_PREFIX/include/x86_64-linux-gnu -I/usr/include/x86_64-linux-gnu"
+# export CPPFLAGS="$CPPFLAGS -I$FFBUILD_PREFIX/include -I$FFBUILD_PREFIX/include/x86_64-linux-gnu -I/usr/include/x86_64-linux-gnu"
+export CPPFLAGS="$CPPFLAGS -I$FFBUILD_PREFIX/include"
 export PKG_CONFIG_PATH="/usr/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
 
 export LEPTONICA_CFLAGS="/usr/include/leptonica:$LEPTONICA_CFLAGS"
